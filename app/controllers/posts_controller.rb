7a361552416e8a489fa736cab317f51a0a65b750
Hanna Lee <mani21na@gmail.com>
2020-02-29 17:25:53 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,13 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
+<<<<<<< HEAD
 	  #permit is for extra protection of security.
 	  #require is for the key of nested.
 	  @post.update(params.require(:post).permit(:title, :description))
+=======
+	  @post.update(params)
+>>>>>>> 094c0c4b584de35cb6bc67ba91bf3c693e76ff0e
 	  redirect_to post_path(@post)
 	end
 end
